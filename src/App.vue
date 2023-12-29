3dd731d6cc7383bcb52c8bf7638264b02e138aae
Gabulon <gabo.fajardo7@gmail.com>
2023-12-28 18:38:03 -0600

    Single file components (SFA) en vue.js

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <script setup>
-
+const hola = "hola Mundo"
 </script>
 
 <template>
   
+  <h1>{{ hola }}</h1>
   
 </template>
 
 <style scoped>
-
+h1{
+  text-transform: uppercase;
+  color:red;
+}
 </style>
