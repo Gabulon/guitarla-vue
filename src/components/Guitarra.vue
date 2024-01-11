47b055aca1d441489cdcb452fdd5668fa5252716
Gabulon <gabo.fajardo7@gmail.com>
2024-01-11 16:31:55 -0600

    eventos en Vue.js Inline y Method

--- src/components/Guitarra.vue
+++ src/components/Guitarra.vue
@@ -1,10 +1,15 @@
 <script setup>
+import {ref}from 'vue'
+const numero=ref(0)
 const props = defineProps({
     guitarra:{
         type:Object,
         required:true
     }
 })
+const incrementar = ()=>{
+    numero.value++
+}
 </script>
 <template>
     <div class="col-md-6 col-lg-4 my-4 row align-items-center">
@@ -15,11 +20,13 @@ const props = defineProps({
         </div>
         <div class="col-8">
             <h3 class="text-black fs-4 fw-bold text-uppercase">{{ guitarra.nombre }}</h3>
+            <p>{{ numero }}</p>
             <p>{{ guitarra.descripcion }}</p>
             <p class="fw-black text-primary fs-3">{{ guitarra.precio }}</p>
             <button 
                 type="button"
                 class="btn btn-dark w-100 "
+                v-on:click="incrementar"
             >Agregar al Carrito</button>
         </div>
     </div><!-- FIN GUITARRA -->
